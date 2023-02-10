9f814de35f31e2acd663955a20b9c2e42324ef5f
Aries-kob <aries-kob@ezweb.ne.jp>
2023-02-10 16:38:44 +0000

    フリマサンプルアプリ作成

---
+++ db/migrate/20230208115833_create_destinations.rb
@@ -0,0 +1,14 @@
+class CreateDestinations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :destinations do |t|
+      
+      t.integer :customer_id, null: false
+      t.string :shipping_postcode, null: false
+      t.string :shipping_address, null: false
+      t.string :direction, null: false
+
+
+      t.timestamps
+    end
+  end
+end
